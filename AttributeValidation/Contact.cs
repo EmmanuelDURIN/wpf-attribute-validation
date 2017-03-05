7467a37db1ce6ef129d0e7c75002f2f69074f026
Emmanuel DURIN <e_durin@hotmail.com>
2017-03-05 15:18:09 +0100

    Initial Commit

---
+++ AttributeValidation/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeValidation
+{
+    public class Contact : ValidatorBase
+    {
+    [Required(ErrorMessage = " Name is required.")]
+    [StringLength(50, ErrorMessage = "No more than 50 characters")]
+    [Display(Name = "Name")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [StringLength(50, ErrorMessage = "No more than 50 characters")]
+    [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Valid email required e.g. abc@xyz.com")]
+    public string Email { get; set; }
+
+    [Display(Name = "Phone Number")]
+    [Required]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+          ErrorMessage = "Entered phone format is not valid.")]
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+                        ErrorMessage = "Entered phone format is not valid.")]
+    public string Mobile { get; set; }
+
+  }
+}
---
+++ AttributeValidation/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttributeValidation
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Contact contact = new Contact();
+    public MainWindow()
+    {
+      InitializeComponent();
+      DataContext = contact;
+    }
+  }
+}
